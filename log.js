14ed979d8de6516df1ae6d6d328e0a562e47e8d1
Mekhla <mekhlasen2005@gmail.com>
2024-07-01 23:29:10 +0530

    natural logarithm of 2

--- log.js
+++ log.js
@@ -1,13 +1,12 @@
 let s = 0;
 for (i = 1; i <= 5; i++) {
     if (i % 2 == 0) {
-        s =s- 1 / i;
+        s = s - 1 / i;
         s = parseFloat(s);
     }
     else {
-        s = s+ 1 / i;
+        s = s + 1 / i;
         s = parseFloat(s);
     }
-    
 }
 console.log(s);
