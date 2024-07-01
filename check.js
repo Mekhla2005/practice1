7bbd9414a0ed5fa46a60161cd7d431684a1f905e
Mekhla <mekhlasen2005@gmail.com>
2024-07-01 23:20:54 +0530

    print numbers divisible by 9 between 100 to 200

---
+++ check.js
@@ -0,0 +1,10 @@
+let sum=0;
+for (i = 101; i <200; i++) 
+    {
+        if(i%9==0)
+            {
+                console.log(i);
+                sum=sum+i;
+            }
+    }
+    console.log(sum);
